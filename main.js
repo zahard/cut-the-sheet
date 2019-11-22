428eaad58d19071d1d9b4e1f941078dc0371e0f8
Zakhar Portianov <kkragy@gmail.com>
2019-11-23 00:52:15 +0200

    Try re-grouping before cut

--- main.js
+++ main.js
@@ -6,6 +6,8 @@ function Rect(x, y, w, h) {
   this.h = h;
 }
 
+var cuts = [];
+
  /*
   *  Vertical cut of current node 
   *  Current Node is ABCD
@@ -24,22 +26,29 @@ function Rect(x, y, w, h) {
   *      C----F-------D
   * 
   */
-Rect.prototype.cutVertical = function(cutPosX) {
+Rect.prototype.cutVertical = function(cutPosX, cutWidth) {
+  cuts.push(this.h)
+  cutWidth = cutWidth || 0;
   return [
     new Rect(this.x, this.y, cutPosX, this.h),
-    new Rect(this.x + cutPosX, this.y, this.w - cutPosX, this.h)
+    new Rect(this.x + cutPosX + cutWidth, this.y, this.w - cutPosX - cutWidth, this.h)
   ];
 }
 
-Rect.prototype.cutHorizontal = function(cutPosY) {
+Rect.prototype.cutHorizontal = function(cutPosY, cutWidth) {
+  cuts.push(this.w)
+  cutWidth = cutWidth || 0;
   return [
     new Rect(this.x, this.y, this.w, cutPosY),
-    new Rect(this.x, this.y + cutPosY, this.w, this.h - cutPosY)
+    new Rect(this.x, this.y + cutPosY + cutWidth, this.w, this.h - cutPosY - cutWidth)
   ];
 }
 
 Rect.prototype.fit = function(tile) {
   return this.w >= tile.width && this.h >= tile.height
+}
+Rect.prototype.fitAny = function(tile) {
+  return this.w >= tile.width && this.h >= tile.height || this.w >= tile.height && this.h >= tile.width;
 }
 Rect.prototype.fitExactly = function(tile) {
   return this.w === tile.width && this.h === tile.height;
@@ -69,12 +78,24 @@ class Node {
     // Tile already setted for this node, return up
     if (this.tileId) return null;
     
-    // if we're too small, return)
-    if (!this.rect.fit(tile)) return null;
+    // if we're too small, return
+    if (!this.rect.fit(tile)) {
+      if (this.rect.fit(tile.rotated())) {
+        tile.rotate();
+      } else {
+        return null;
+      }
+    }
 
     // If node already match exactly target tile - return it
     // this is SUCCESS point of insertion
     if (this.rect.fitExactly(tile)) {
+      this.tileId = tile.id;
+      return this;
+    }
+
+    if (this.rect.fitExactly(tile.rotated())) {
+      tile.rotate();
       this.tileId = tile.id;
       return this;
     }
@@ -87,9 +108,11 @@ class Node {
     var dw = this.rect.w - tile.width;
     var dh = this.rect.h - tile.height;
 
+    var cutWidth = 0.2
+
     var cuts = dw > dh 
-      ? this.rect.cutVertical(tile.width)
-      : this.rect.cutHorizontal(tile.height);
+      ? this.rect.cutVertical(tile.width, cutWidth)
+      : this.rect.cutHorizontal(tile.height, cutWidth);
 
     this.left.rect = cuts[0];
     this.right.rect = cuts[1];
@@ -98,35 +121,124 @@ class Node {
   }
 }
 
+
+
 function Tile(id, width, height) {
   this.id = id;
   this.width = width;
   this.height = height;
 }
 
+Tile.prototype.rotated = function() {
+  return new Tile(this.id, this.height, this.width);
+}
+
+Tile.prototype.rotate = function() {
+  var tmp = this.width;
+  this.width = this.height;
+  this.height = tmp;
+}
+
 function sortTiles(tiles) {
   var tmp = tiles.slice();
   tmp.sort((a, b) => {
-    return Math.max(b.width, b.height) - Math.max(a.width, a.height);
+    //var d = Math.max(b.width, b.height) - Math.max(a.width, a.height);
+    var d = (b.width + b.height) - (a.width + a.height);
+    if (d == 0) {
+      return b.width * b.height - a.width * a.height;
+    } else {
+      return d;
+    }
   });
   return tmp;
 }
 
 function main() {
-  var basePlate = new Rect(0, 0, 200, 120);
-  var tilez = [
-    new Tile('T2', 90, 25),
+  var basePlate = new Rect(0, 0, 202, 120);
+  var tiles = sortTiles([
     new Tile('T0', 150, 40),
-    new Tile('T1', 30, 100),
-    new Tile('T3', 50, 30),
+    new Tile('T1', 100, 30),
+    new Tile('T2', 70, 30),
+    new Tile('T4', 70, 40),
     new Tile('T3', 80, 40),
-  ];
+  ]);
   
-  tiles = sortTiles(tilez);
+  var sourceTiles = tiles;
 
+  var groupTiles = false;
+  if (groupTiles) {
+    var groups = [];
+    // Group Similar
+    for (var i = 0; i < tiles.length; i++) {
+      if (tiles[i].grouped) continue;
+      
+      var group = {
+        tiles: [tiles[i]],
+        h: tiles[i].height,
+        w: tiles[i].width,
+      };
+      
+      for (var j = i + 1; j < tiles.length; j++) {
+        if (tiles[j].height === group.h) {
+          if (tiles[j].width + group.w <= basePlate.w) {
+            group.tiles.push(tiles[j]);
+            group.w += tiles[j].width;
+            tiles[j].grouped = true;
+          }
+        }
+      }
+      
+      groups.push(group);
+    }
+
+    // Collect groups 
+    var groupsNotSingle = [];
+    for (var i=0;i < groups.length; i++) {
+      if (groups[i].tiles.length > 1) {
+        groupsNotSingle.push(i);
+      }
+    }
+
+    // Ungroup one by one withall possible variations
+    var ungroupIndex = 1;
+    var groupIndex = groupsNotSingle[ungroupIndex];
+    var groupsRoll = [];
+
+    for (var i = 0; i < groups.length; i++) {
+      if (i !== groupIndex) {
+        groupsRoll.push(groups[i]);
+      } else {
+        groups[i].tiles.forEach(t => {
+          groupsRoll.push({
+            tiles: [t],
+            h: t.height,
+            w: t.width,
+          });
+        });
+      }
+    }
+    
+    groupsRoll.sort((a, b) => {
+      var d = (b.w + b.h) - (a.w + a.h);
+      if (d == 0) {
+        return b.w * b.h - a.w * a.h;
+      } else {
+        return d;
+      }
+    });
+    
+    // Create tiles based on groups
+    tiles = groupsRoll.map((g,i) => {
+      return new Tile('Group-'+i, g.w, g.h);
+    });
+
+    console.log(tiles)
+  }
+    
   var root = new Node(basePlate);
   tiles.forEach(tile => {
     var res = root.insert(tile);
+
     if (!res) {
       // Try to rotate tile
       var tmp = tile.width;
@@ -166,7 +278,7 @@ function createSheet(parent,vw, vh, node) {
     d.innerText = node.tileId;
   }
   if (node.left || node.right) {
-    d.classList.add('contais');
+    d.classList.add('contains');
   }
   d.style.left = node.rect.x / vw + '%';
   d.style.top = node.rect.y / vh + '%';
